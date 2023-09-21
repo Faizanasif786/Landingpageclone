1183f3788e8d5c7bd6f199c0533f079eab09ffc2
Muhammad Faizan Asif <126105736+Faizanasif786@users.noreply.github.com>
2023-09-21 05:08:43 -0700

    Add files via upload

---
+++ use.js
@@ -0,0 +1,24 @@
+function dark(){
+    document.getElementById('darkbody').style.backgroundColor='black'
+    document.getElementById('btn-text').style.backgroundColor='white'
+    document.getElementById('btn-text').style.color='black'
+    document.getElementById('sec-one-back').style.backgroundColor = 'black'
+    document.getElementById('sec-one-back').style.color='#fff'
+    document.getElementById('third-sec').style.color='#fff'
+    document.getElementById('fourth').style.backgroundColor='black'
+    document.getElementById('fourth').style.color='#fff'
+    document.getElementById('last').style.backgroundColor='black'
+    document.getElementById('last').style.color='#fff'
+}
+function light(){
+    document.getElementById('darkbody').style.backgroundColor=''
+    document.getElementById('btn-text').style.backgroundColor=''
+    document.getElementById('btn-text').style.color=''
+    document.getElementById('sec-one-back').style.backgroundColor = ''
+    document.getElementById('sec-one-back').style.color=''
+    document.getElementById('third-sec').style.color=''
+    document.getElementById('fourth').style.backgroundColor=''
+    document.getElementById('fourth').style.color=''
+    document.getElementById('last').style.backgroundColor=''
+    document.getElementById('last').style.color=''
+}
